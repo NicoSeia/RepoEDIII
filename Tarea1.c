8de88baa1a6d69e216da1eb4856c602fbff2281c
NicoSeia <nicolasseia0@gmail.com>
2024-08-09 11:22:09 -0300

    Tarea 1 lista

---
+++ Tarea1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// Tabla del 8
+
+int main(){
+
+    int num = 8;
+
+    printf("----------Tabla del 8----------\n");
+
+    for(int i=1; i<=10; i++){
+        printf("%d * %d = %d\n", i, num, i * num);
+    }
+
+    printf("-------------------------------");
+
+    return 0;
+}
